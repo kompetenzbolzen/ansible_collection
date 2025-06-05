9bddb1393ad683f49ce7af4ecea855f45ec3b352
Jonas Gunz <himself@jonasgunz.de>
2025-06-05 15:53:42 +0200

    factorio fix multiple servers

--- roles/factorio/files/run.sh
+++ roles/factorio/files/run.sh
@@ -8,10 +8,13 @@ mkdir -p ./saves
 
 if [ ! -f "./saves/$SAVEFILE.zip" ]; then
 	factorio/bin/x64/factorio --create "./saves/$SAVEFILE.zip" \
+		--config ./"$CFGDIR"/config.ini \
 		--map-gen-settings ./"$CFGDIR"/mapgen-config.json \
 		--map-settings ./"$CFGDIR"/map-config.json || exit 1
 fi
 
 factorio/bin/x64/factorio \
+	--config ./"$CFGDIR"/config.ini \
 	--start-server "./saves/$SAVEFILE.zip" \
-	--server-settings "$CFGDIR"/server-config.json
+	--server-settings "$CFGDIR"/server-config.json \
+	--port "$PORT"
